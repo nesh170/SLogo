b06873eb0ba8cb4fec123792d65be6ad698da125
nesh170 <sivaneshwaran.loganathan@duke.edu>
2015-02-17 19:19:23 -0500

    Started work on View, added the CodePane. consists of terminal,codebox and enter button. Added public methods for interaction

--- src/sLogo_team02/Main.java
+++ src/sLogo_team02/Main.java
@@ -1,14 +1,19 @@
 package sLogo_team02;
 
+import view.ViewAbstract;
+import view.ViewFX;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	public static final String TITLE ="SLOGO Team_02";
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void start(Stage s) throws Exception {
+		ViewAbstract view = new ViewFX();
+		s.setScene(view.initializeView());
+		s.setTitle(TITLE);
+		s.show();
 	}
 	
 	public static void main(String[] args){
