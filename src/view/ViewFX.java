ac72f4c3b331389afc67e35c71a018c66f7fb43a
nesh170 <sivaneshwaran.loganathan@duke.edu>
2015-02-20 14:39:27 -0500

    commented out view test method

--- src/view/ViewFX.java
+++ src/view/ViewFX.java
@@ -41,7 +41,7 @@ public class ViewFX extends ViewAbstract {
 		myVariableElements = new VariablePane(myRoot);
 		myRoot.getChildren().addAll(myLineRoot, myTurtleRoot);
 		myController.setScene(viewScene);
-		test();
+//		test();
 	}
 
 	
