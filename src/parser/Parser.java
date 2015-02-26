01d4bb991ea563bf85c6f4c3b2874f7475d1e0a7
Sierra Smith <sms95@duke.edu>
2015-02-25 21:03:36 -0500

    Added ability for parser to handle parsing groups

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -13,7 +13,7 @@ public class Parser {
 	private static final String OPEN_BRACKET = "[";
 	private static final String CLOSED_BRACKET = "]";
 	private static final String GROUP = "Group";
-	
+
 	private Regex myRegex;
 	private int myCurIndex;
 	private String[] myCurProgramArray;
@@ -28,15 +28,15 @@ public class Parser {
 	}
 	//returns a program, once that class is created
 	public List<ParseNode> parse(String program){
-//		preProcess the string;
+		//		preProcess the string;
 		String processed = preProcessString(program);
 		if(processed == null){
 			return null;
 		}
 		myCurProgramArray = processed.split(" ");
-//		create a program;
+		//		create a program;
 		List<ParseNode> topNodes = new ArrayList<>();
-//		loop through the string array until it's empty{
+		//		loop through the string array until it's empty{
 		while(myCurIndex < myCurProgramArray.length-1){
 			ParseNode curNode = new ParseNode(myCurProgramArray[++myCurIndex]);
 			System.out.println("Root Node: " + curNode.getName());
@@ -45,58 +45,61 @@ public class Parser {
 			topNodes.add(processedNode);
 			System.out.println("Added node: " + processedNode.getName());
 		}
-//			create new node; //the index of the string is index + 1
-//			increment index;
-//			call parsenode on the current node;
-//			add current node (head node of each independent command) to the statement list; 
-//		}
-//		make and return program;
+		//			create new node; //the index of the string is index + 1
+		//			increment index;
+		//			call parsenode on the current node;
+		//			add current node (head node of each independent command) to the statement list; 
+		//		}
+		//		make and return program;
 		myCurIndex = -1;
 		return topNodes;
 	}
-	
+
 	public void changeLanguage(String language){
-		 
+
 	}
-	
+
 	public String preProcessString(String program){
 		String[] splitProgram = program.split(" ");
-//		check if string is empty
+		//		check if string is empty
 		if(splitProgram.length < 1 | program.length() < 1){
 			System.out.println("String empty or full of spaces");
 			return null;
 		}
-		
+
 		//pre-process the string to remove comments
 		return program;
 	}
-	
+
 	private ParseNode recursiveCommandBuilder(ParseNode current){
 		String nodeName = current.getName();
-		
+
 		String type = myRegex.matchSyntax(nodeName);
-		
+
 		switch(type){
 		case "Command":
 			String commandType = myRegex.matchCommand(nodeName);
 			//check if commmandtype is null, this means it may be a user defined command and call appropriate helper
 			if(commandType == null){
 				//check if user defined method and act accordingly
-				System.out.println("Command type is null");
-				return null;
+				if(nodeName.equals(GROUP)){
+					commandType = GROUP;
+				}
+				else{
+					System.out.println("Command type is null");
+					return null;
+				}
 			}
-			 int loopTimes = 0;
-             try {
-                 loopTimes = Constants.getNumParam(commandType);
-             }
-             catch (IllegalArgumentException e) {
-                 // TODO Auto-generated catch block
-                 e.printStackTrace();
-             }
-             catch (IllegalAccessException e) {
-                 // TODO Auto-generated catch block
-                 e.printStackTrace();
-             }
+			int loopTimes = 0;
+			try {
+				loopTimes = Constants.getNumParam(commandType);
+			}
+			catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			}
+			catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
 			switch(commandType){
 			case USER_DEFINED:
 				loopTimes = myProgMethodsAndParams.get(nodeName);
@@ -116,12 +119,15 @@ public class Parser {
 				System.out.println("Added variable to variable set. "+ myProgramVariables.contains(variable));
 				retrieveChildren(current, loopTimes);
 				break;
+			case GROUP:
+				getGroupKids(current);
+				break;
 			default:
 				retrieveChildren(current, loopTimes);
 				break;
 			}
 			return current;
-			
+
 		case "Constant":
 			return current;
 		case "Variable":
@@ -131,14 +137,43 @@ public class Parser {
 		default:
 			System.out.println("YOU SCREWED UP SOMEWHERE");
 		}
-		
 		return null;
+	}
+
+	//duplicated with method below, refactor
+	private void getGroupKids(ParseNode groupLeader){
+		boolean groupComplete = false;
+		while(!groupComplete){
+			if(atEndOfString()){
+				System.out.println("Missing parameters for a something in a group");
+				//NOTE, ADD ERROR CHECKING HERE, WANT TO THROW ERROR
+				return;
+			}
+			String next = myCurProgramArray[++myCurIndex];
+			if(next.equals(CLOSED_BRACKET)){
+				System.out.println("Group ended");
+				groupComplete = true;
+			}
+			else if(next.equals(OPEN_BRACKET)){
+				ParseNode newGroup = new ParseNode(GROUP);
+				System.out.println("New Node: " + newGroup.getName());
+				ParseNode processedNode = recursiveCommandBuilder(newGroup);
+				groupLeader.addChild(processedNode);
+			}
+			else{
+				ParseNode newNode = new ParseNode(next);
+				System.out.println("New Node: " + newNode.getName());
+				ParseNode processedNode = recursiveCommandBuilder(newNode);
+				groupLeader.addChild(processedNode);
+				System.out.println("Added node: " + processedNode.getName());
+			}
+		}
 	}
 	
 	private void retrieveChildren(ParseNode current, int loopTimes) {
 		while(current.getNumChildren() < loopTimes){
 			if(atEndOfString()){
-				System.out.println("Missing parameters for a command");
+				System.out.println("Missing parameters for command: " + current.getName());
 				//NOTE, ADD ERROR CHECKING HERE, WANT TO THROW ERROR
 				return;
 			}
@@ -161,11 +196,11 @@ public class Parser {
 			}
 		}
 	}
-	
+
 	private boolean atEndOfString(){
 		return (myCurIndex == myCurProgramArray.length-1);
 	}
-	
-	
-	
+
+
+
 }
