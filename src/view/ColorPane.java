d2672c6c07effdc5c60b67b4b419595c31007ee1
Jangsoon <jp148@duke.edu>
2015-03-06 00:51:09 -0500

    colorpane

--- src/view/ColorPane.java
+++ src/view/ColorPane.java
@@ -1,5 +1,32 @@
 package view;
 
-public class ColorPane {
+import java.util.ArrayList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Group;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
 
+public class ColorPane {
+	private ObservableList<String> myColorList;
+	private ListView<String> myColorListView;
+	
+	public ColorPane(Group root, ArrayList<String> colorList) {
+		initializeSuggestedView(root, colorList);		
+	}
+	
+	private void initializeSuggestedView(Group root, ArrayList<String> colorList) {
+		myColorList = FXCollections.observableArrayList(colorList);
+		VBox colorBox = new VBox();
+	    myColorListView.setItems(myColorList);
+	    colorBox.getChildren().add(myColorListView);
+		root.getChildren().add(colorBox);
+	}
+	
+	public void changeList(ArrayList<String> colorList) {
+		myColorList = FXCollections.observableArrayList(colorList);
+		myColorListView.setItems(myColorList);
+	}
+	
+	
 }
