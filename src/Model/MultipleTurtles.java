612ce51ef9ce18f681a0efb2f57e6f2a1d75ff52
Sierra Smith <sms95@duke.edu>
2015-03-07 03:10:29 -0500

    Added comments to pen

--- src/Model/MultipleTurtles.java
+++ src/Model/MultipleTurtles.java
@@ -9,12 +9,18 @@ import java.util.function.Consumer;
 import view.*;
 import Constants.*;
 
+/**
+ */
 public class MultipleTurtles implements ITurtle {
 	private Map<Integer, SingleTurtle> myTurtles;
 	private List<Integer> myActiveTurtleIDs;
 	private List<SingleTurtle> myActiveTurtles;
 	private ViewAbstract myView;
 
+	/**
+	 * Constructor for MultipleTurtles.
+	 * @param view ViewAbstract
+	 */
 	public MultipleTurtles(ViewAbstract view){
 		myTurtles = new HashMap<>();
 		myActiveTurtleIDs = new ArrayList<>();
@@ -22,6 +28,10 @@ public class MultipleTurtles implements ITurtle {
 		myView = view;
 	}
 
+	/**
+	 * Method setActiveTurtles.
+	 * @param activeTurtleIDs List<Integer>
+	 */
 	public void setActiveTurtles(List<Integer> activeTurtleIDs){
 		clearActiveTurtleIDs();
 		clearActiveTurtleList();
@@ -51,22 +61,44 @@ public class MultipleTurtles implements ITurtle {
 	}
 
 	//should return immutable list instead
+	/**
+	 * Method getActiveTurtleIDs.
+	 * @return List<Integer>
+	 */
 	public List<Integer> getActiveTurtleIDs(){
 		return myActiveTurtleIDs;
 	}
 	
+	/**
+	 * Method getActiveTurtles.
+	 * @return List<SingleTurtle>
+	 */
 	public List<SingleTurtle> getActiveTurtles(){
 		return myActiveTurtles;
 	}
 
+	/**
+	 * Method doToActiveTurtles.
+	 * @param action Consumer<? super SingleTurtle>
+	 * @see Model.ITurtle#doToActiveTurtles(Consumer<? super SingleTurtle>)
+	 */
 	public void doToActiveTurtles(Consumer<? super SingleTurtle> action){
 		myActiveTurtles.forEach(action);
 	}
 	
+	/**
+	 * Method doToSpecifiedTurtles.
+	 * @param turtles List<SingleTurtle>
+	 * @param action Consumer<? super SingleTurtle>
+	 */
 	private void doToSpecifiedTurtles(List<SingleTurtle> turtles, Consumer<? super SingleTurtle> action){
 		turtles.forEach(action);
 	}
 
+	/**
+	 * Method addTurtle.
+	 * @param turtleID Integer
+	 */
 	public void addTurtle(Integer turtleID){
 		myTurtles.put(turtleID, new SingleTurtle(turtleID));
 		//automatically activates new turtles
@@ -75,16 +107,31 @@ public class MultipleTurtles implements ITurtle {
 		myView.addShape(Constants.DEFAULT_SHAPE, 0, 0, turtleID);
 	}
 	
+	/**
+	 * Method addTurtle.
+	 * @param X double
+	 * @param Y double
+	 * @param turtleID int
+	 */
 	public void addTurtle(double X, double Y, int turtleID){
 	    addTurtle(turtleID);
 	    myTurtles.get(turtleID).setX(X);myTurtles.get(turtleID).setY(Y);
 	    myView.moveShape(X, Y, turtleID);
 	}
 
+	/**
+	 * Method getTurtle.
+	 * @param ID Integer
+	 * @return SingleTurtle
+	 */
 	public SingleTurtle getTurtle(Integer ID){
 		return myTurtles.get(ID);
 	}
 	
+	/**
+	 * Method toggleTurtle.
+	 * @param ID Integer
+	 */
 	public void toggleTurtle(Integer ID){
 		boolean found = false;
 		for(Integer i: myActiveTurtleIDs){
@@ -106,54 +153,118 @@ public class MultipleTurtles implements ITurtle {
 	}
 	//ITurtle Methods
 	
+	/**
+	 * Method moveTurtle.
+	 * @param distance double
+	 * @param angle double
+	 * @return double
+	 * @see Model.ITurtle#moveTurtle(double, double)
+	 */
 	public double moveTurtle(double distance, double angle){
 		doToActiveExceptFirst(e -> e.moveTurtle(distance, angle));
 		return myActiveTurtles.get(0).moveTurtle(distance, angle);
 	}
 	
+	/**
+	 * Method relocateTurtle.
+	 * @param x double
+	 * @param y double
+	 * @return double
+	 * @see Model.ITurtle#relocateTurtle(double, double)
+	 */
 	public double relocateTurtle(double x, double y){
 		doToActiveExceptFirst(t -> t.relocateTurtle(x, y));
 		return myActiveTurtles.get(0).relocateTurtle(x, y);
 	}
 	
+	/**
+	 * Method setShapeIndex.
+	 * @param index int
+	 * @see Model.ITurtle#setShapeIndex(int)
+	 */
 	public void setShapeIndex(int index){
 		myActiveTurtles.forEach(e -> e.setShapeIndex(index));
 	}
 	
+	/**
+	 * Method setHiding.
+	 * @param hiding boolean
+	 * @see Model.ITurtle#setHiding(boolean)
+	 */
 	public void setHiding(boolean hiding){
 		myActiveTurtles.forEach(e -> e.setHiding(hiding));
 	}
 	
+	/**
+	 * Method setDrawing.
+	 * @param drawing boolean
+	 * @see Model.ITurtle#setDrawing(boolean)
+	 */
 	public void setDrawing(boolean drawing){
 		myActiveTurtles.forEach(e -> e.setDrawing(drawing));
 	}
+	/**
+	 * Method setPenColor.
+	 * @param index int
+	 * @see Model.ITurtle#setPenColor(int)
+	 */
 	public void setPenColor(int index){
 		myActiveTurtles.forEach(e -> e.setPenColor(index));
 	}
 	
+	/**
+	 * Method setPenSize.
+	 * @param pixels double
+	 * @see Model.ITurtle#setPenSize(double)
+	 */
 	public void setPenSize(double pixels){
 		myActiveTurtles.forEach(e -> e.setPenSize(pixels));
 	}
 	
+	/**
+	 * Method jump.
+	 * @param x double
+	 * @param y double
+	 * @see Model.ITurtle#jump(double, double)
+	 */
 	public void jump(double x, double y){
 		myActiveTurtles.forEach(e -> e.jump(x, y));
 	}
 	
+	/**
+	 * Method rotate.
+	 * @param angle double
+	 * @see Model.ITurtle#rotate(double)
+	 */
 	public void rotate(double angle){
 		myActiveTurtles.forEach(e -> e.rotate(angle));
 	}
 	
+	/**
+	 * Method setAngle.
+	 * @param angle double
+	 * @return double
+	 * @see Model.ITurtle#setAngle(double)
+	 */
 	public double setAngle(double angle){
 		doToActiveExceptFirst(e -> e.setAngle(angle));
 		return myActiveTurtles.get(0).setAngle(angle);
 	}
 	
+	/**
+	 * Method doToActiveExceptFirst.
+	 * @param action Consumer<? super SingleTurtle>
+	 */
 	private void doToActiveExceptFirst(Consumer<? super SingleTurtle> action){
 		List<SingleTurtle> activeCopy = new ArrayList<>(myActiveTurtles);
 		activeCopy.remove(0);
 		doToSpecifiedTurtles(activeCopy, action);
 	}
 	
+	/**
+	 * Method getTurtleMap.
+	 * @return Map<Integer,SingleTurtle>
+	 */
 	public Map<Integer, SingleTurtle> getTurtleMap(){
 	    return Collections.unmodifiableMap(myTurtles);
 	}
