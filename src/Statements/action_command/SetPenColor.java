7d24bc80c97f78125862a99437c283e6fc4c4255
Sierra Smith <sms95@duke.edu>
2015-03-06 22:13:13 -0500

    Added abstract set visual command to elimiate duplicated code in set shape and set pen color

--- src/Statements/action_command/SetPenColor.java
+++ src/Statements/action_command/SetPenColor.java
@@ -7,27 +7,18 @@ import Statements.Statement;
 import view.*;
 import exceptions.*;
 
-public class SetPenColor extends ActionCommand{
-	private List<String> myColors;
+public class SetPenColor extends SetVisual{
 
 	public SetPenColor(List<Statement> statements, ViewAbstract view, ITurtle turtles, List<String> colors) {
-		super(statements, view, turtles);
-		myColors = colors;
+		super(statements, view, turtles, colors);
+		setErrorMessage("Invalid index for for set pen color.");
 	}
 
 	@Override
-	public double execute() {
-		int index = (int) myStatements.get(0).execute();
-		try{
-			if(index >= myColors.size() || index < 0){
-				throw new ExecutionException("Invalid index for set pen color.");
-			}else{
-				myTurtles.setPenColor(index);
-			}
-		}catch(ExecutionException e){
-			myView.printError(e.toString());
-		}
-		return (double) index;
+	public void completeOperation(int index) {
+		myTurtles.setPenColor(index);
 	}
 
+	
+	
 }
---
+++ src/Statements/action_command/SetVisual.java
@@ -0,0 +1,46 @@
+package Statements.action_command;
+
+import java.util.List;
+
+import exceptions.ExecutionException;
+import view.ViewAbstract;
+import Model.ITurtle;
+import Statements.Statement;
+
+public abstract class SetVisual extends ActionCommand {
+	private List<String> myVisualList;
+	private String myErrorMessage;
+	
+	public SetVisual(List<Statement> statements, ViewAbstract view,
+			ITurtle turtleManager, List<String> listOfVisualItems) {
+		super(statements, view, turtleManager);
+		myVisualList = listOfVisualItems;
+		myErrorMessage = "Invalid index.";
+	}
+
+	@Override
+	public double execute() {
+		int index = (int) myStatements.get(0).execute();
+		try{
+			if(index >= myVisualList.size() || index < 0){
+				throw new ExecutionException(myErrorMessage);
+			}else{
+				completeOperation(index);
+			}
+		}catch(ExecutionException e){
+			myView.printError(e.toString());
+		}
+		return (double) index;
+	}
+	
+	public void setErrorMessage(String message){
+		myErrorMessage = message;
+	}
+
+	public abstract void completeOperation(int index);
+	
+	public List<String> getVisualList(){
+		return myVisualList;
+	}
+	
+}
