d977d2b73daf3c0b2f6ff68b3ca7096a2dddc8e7
nesh170 <sivaneshwaran.loganathan@duke.edu>
2015-03-07 04:25:59 -0500

    added changes

--- src/parser/ForNode.java
+++ src/parser/ForNode.java
@@ -2,18 +2,23 @@ package parser;
 
 import exceptions.ParserException;
 
+
+/**
+ * @author Sierra, Yancheng
+ *
+ */
 public class ForNode extends CommandNode {
 
-	public ForNode(String name, Parser parser, String commandType) {
-		super(name, parser, commandType);
-	}
-	
-	@Override
-	public ParseNode finishProcessing() throws ParserException{
-		String variableForLoop = myParser.getElementAtIndex(myParser.getCurIndex()+2);
-		myParser.addVariableToTable(variableForLoop);
-		myParser.retrieveChildren(this, getNumParams());
-		return this;
-	}
+    public ForNode (String name, Parser parser, String commandType) {
+        super(name, parser, commandType);
+    }
+
+    @Override
+    public ParseNode finishProcessing () throws ParserException {
+        String variableForLoop = myParser.getElementAtIndex(myParser.getCurIndex() + 2);
+        myParser.addVariableToTable(variableForLoop);
+        myParser.retrieveChildren(this, getNumParams());
+        return this;
+    }
 
 }
