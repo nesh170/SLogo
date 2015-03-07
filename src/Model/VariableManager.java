65d4b1e3fc919420404430d9e6e21d526bdd4b8b
Sierra Smith <sms95@duke.edu>
2015-03-07 03:50:02 -0500

    added comments to variable manager

--- src/Model/VariableManager.java
+++ src/Model/VariableManager.java
@@ -4,20 +4,40 @@ import java.util.HashMap;
 import view.*;
 import java.util.Map;
 
+/**
+ * This class keeps track of all of the variables currently available in whatever class is tracking
+ * this manager.  Allows functionality to add a variable with a corresponding value and get the 
+ * value given a variable name.  
+ * 
+ * @author Sierra, Yancheng
+ */
 public class VariableManager {
 	private Map<String, Double> myVariables;
 	private ViewAbstract myView;
 	
+	/**
+	 * Constructor for VariableManager.
+	 * @param view ViewAbstract
+	 */
 	public VariableManager(ViewAbstract view){
 		myVariables = new HashMap<>();
 		myView = view;
 	}
 	
+	/**
+	 * Adds a variable with the given name and value
+	 * @param var String
+	 * @param value Double
+	 */
 	public void addVariable(String var, Double value){
 		myVariables.put(var, value);
 		myView.addVariable(var, value);
 	}
 	
+	/**
+	 *Removes the variable with the given name if it exists
+	 * @param var String
+	 */
 	public void removeVariable(String var){
 		myVariables.remove(var);
 	}
@@ -26,10 +46,20 @@ public class VariableManager {
 		myVariables.clear();
 	}
 	
+	/**
+	 * Returns the value of the specified variable, assumes the variable exists
+	 * @param varName String
+	 * @return Double
+	 */
 	public Double getVarValue(String varName){
 		return myVariables.get(varName);
 	}
 	
+	/**
+	 * Returns true if a variable with the given name exists, false otherwise.
+	 * @param s String
+	 * @return boolean
+	 */
 	public boolean containsVariable(String s){
 		return myVariables.containsKey(s);
 	}
