8dc4e1fa5c858d4a9d7cf90da3cabeb7de6d5572
nesh170 <sivaneshwaran.loganathan@duke.edu>
2015-04-28 22:16:08 -0400

    refactored the code and fixed the translation bug

--- src/view/TurtleDisplay.java
+++ src/view/TurtleDisplay.java
@@ -1,7 +1,9 @@
 package view;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Callable;
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Shape;
@@ -15,6 +17,7 @@ public class TurtleDisplay {
     private Callable<Map<Integer, Shape>> myCallableToGetTurtles;
     private Runnable myTurtleRenderCaller;
     private Map<Integer, Shape> myShapeMap;
+    private Map<Integer, Point2D> myLocationMap;
 
     public TurtleDisplay (Callable<Map<Integer, Shape>> getTurtleMap, Runnable renderTurtles) {
         myCallableToGetTurtles = getTurtleMap;
@@ -23,6 +26,7 @@ public class TurtleDisplay {
 
     public void createDisplay () {
         Stage stage = new Stage();
+
         VBox turtleBox = new VBox(VBOX_PADDING);
         try {
             myShapeMap = myCallableToGetTurtles.call();
@@ -31,11 +35,25 @@ public class TurtleDisplay {
         catch (Exception e) {
             turtleBox.getChildren().add(new Text(ERROR_MESSAGE_NO_TURTLE));
         }
+        getTranslatesFromMap();
         stage.setScene(new Scene(turtleBox));
         stage.show();
         stage.setOnCloseRequest(closeEvent -> {
+            myShapeMap.forEach((key,shape) -> {
+                shape.setTranslateX(myLocationMap.get(key).getX());
+                shape.setTranslateY(myLocationMap.get(key).getY());
+            });
             stage.close();
             myTurtleRenderCaller.run();
+        });
+    }
+
+    private void getTranslatesFromMap () {
+        myLocationMap = new HashMap<Integer, Point2D>();
+        myShapeMap.forEach((key,shape) -> myLocationMap.put(key, new Point2D(shape.getTranslateX(),shape.getTranslateY())));
+        myShapeMap.values().forEach(shape -> {
+            shape.setTranslateX(0);
+            shape.setTranslateY(0);
         });
     }
 
